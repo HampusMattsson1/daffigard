15eabc8463e49075a807a0c8bca0fc0900914ae7
Hampus Mattsson <hjm@ownit.nu>
2026-08-18 19:34:40 +0200

    some new crap

--- login.js
+++ login.js
@@ -13,8 +13,8 @@ const context = await chromium.launchPersistentContext(`./${profileFolder}`, {
   viewport: { width: 1920, height: 1080 },
 });
 const page = context.pages()[0] ?? (await context.newPage());
-// await page.goto('https://gamecenter.flarie.com/cff63f8b-5eba-4c79-b604-b17b2c5a1a75'); // Dafgård
-await page.goto('https://gamecenter.flarie.com/ceef65b7-325a-4750-9872-af7f6c97ff2a'); // Billys
+await page.goto('https://gamecenter.flarie.com/cff63f8b-5eba-4c79-b604-b17b2c5a1a75'); // Dafgård
+// await page.goto('https://gamecenter.flarie.com/ceef65b7-325a-4750-9872-af7f6c97ff2a'); // Billys
 
 console.log('🔑 Log in, then close the browser window to save the session.');
 await context.waitForEvent('close', { timeout: 0 });
--- scripts/copy-sessions.sh
+++ scripts/copy-sessions.sh
@@ -1,3 +1,6 @@
+# Pre-requisite is to have a container running
+#podman run --rm -e LOW_RESOURCE=true -e PROFILE=robin-billys -e VARIANT=billys --name daff-test ghcr.io/hampusmattsson1/daffigard:0.1.2-robin
+
 # 1. Set your target directory inside the container and local destination
 CONTAINER_NAME="daff-test"
 CONTAINER_DIR="/app"
@@ -6,4 +9,5 @@ LOCAL_DEST="./USER_SESSIONS"
 mkdir -p "$LOCAL_DEST"
 
 # 2. Find matching directories and copy them
-container exec "$CONTAINER_NAME" sh -c "cd $CONTAINER_DIR && tar -cf - user*" | tar -xf - -C "$LOCAL_DEST"
+#container exec "$CONTAINER_NAME" sh -c "cd $CONTAINER_DIR && tar -cf - user*" | tar -xf - -C "$LOCAL_DEST"
+podman cp "$CONTAINER_NAME:$CONTAINER_DIR/user*" "$LOCAL_DEST"
